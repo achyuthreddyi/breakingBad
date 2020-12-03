e05d82423571efa6f1e780469258ba822b50926a
achyuth reddy <ncsachyuth@gmail.com>
2020-12-03 23:30:45 +0530

    created the basic app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react'
+import './App.css'
+import Header from './components/ui/Header'
 
-function App() {
+const App = () => {
+  const [item, setItem] = useState([])
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
